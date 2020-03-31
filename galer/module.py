589f8418280714719cfd0b09867c801d6eddb029
apaajalahx <52324987+apaajalahx@users.noreply.github.com>
2020-03-31 19:55:34 +0800

    Update module.py

--- galer/module.py
+++ galer/module.py
@@ -8,52 +8,49 @@
 import time,json
 import requests as req
 from bs4 import BeautifulSoup as peler
-from urlparse import urlparse
+from urllib3.util import parse_url
 
 def check_array(arr):
     if len(arr) == 0:
         return 0
-    else: 
+    else:
         return 1
 
 def local_time():
-	t = time.localtime()
-	current_time = time.strftime("%H:%M:%S", t)
-	return current_time
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        return current_time
 
 def save(format):
-	s = open("../result/result.txt", "a+")
-	s.write(format+"\n")
+        s = open("../result/result.txt", "a+")
+        s.write(format+"\n")
 
 def formparse(url):
-	ux = req.get(url,headers=headers)
-	get = peler(html_str,ux.text)
-	getform = peler.find_all("input",{"class":"forminput"})
-	for item in getform:
-		name = item.next_sibling.strip()
-		return name
+        ux = req.get(url,headers=headers)
+        get = peler(html_str,ux.text)
+        getform = peler.find_all("input",{"class":"forminput"})
+        for item in getform:
+                name = item.next_sibling.strip()
+                return name
 
-def get_domain(url, Remove_http=True):
-	url = urlparse(url)
-	if Remove_http:
-		result = f"{url.netloc}"
-	else:
-		result = f"{url.netloc}://{url.netloc}"
-	return result
+def get_domain(url):
+                p = parse_url(url)
+                x = p.host
+                return x
 
 def get_user_and_replace(url,user,x):
-	try:
-		if x is None:
-			return 0
-		if '[DOMAIN]' or '[UPPERUSER]' or '[DEFUSER]' in x:
-			if check_array(x):
-				new = []
-				for pwd in x:
-					new.append(pwd)
-			for px in new:
-				px.replace('[DOMAIN]',get_domain(url))
-				px.replace('[UPPERUSER]',user.upper())
-				px.replace('[DEFUSER]',user)
-			return px
-	except:
-		print(colored("seems we got error in function replace for wpbrute"))
+        try:
+                if x is None:
+                        return 0
+                if '[DOMAIN]' or '[UPPERUSER]' or '[DEFUSER]' in x:
+                        if check_array(x):
+                                new = []
+                                for pwd in x:
+                                        new.append(pwd)
+                        for px in new:
+                                px.replace('[DOMAIN]',get_domain(url))
+                                px.replace('[UPPERUSER]',user.upper())
+                                px.replace('[DEFUSER]',user)
+                        return px
+        except:
+                print(colored("seems we got error in function replace for wpbrute"))
