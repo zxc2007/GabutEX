fbccb9c02ae29896920efa85e625088c15407de6
apaajalahx <52324987+apaajalahx@users.noreply.github.com>
2020-03-31 16:44:08 +0800

    Update module.py

--- galer/module.py
+++ galer/module.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup as peler
 from galer.wpbrute import go_brute
 from galer.pretashop import go_exploit
 from galer.wpexploit import wpexploit
+from urlparse import urlparse
 
 def check_array(arr):
     if len(arr) == 0:
@@ -60,3 +61,28 @@ def exploit_other(url):
 		go_exploit(url)
 	except:
 		print(colored("Seems we got error in function exploit",'red'))
+
+def get_domain(url, Remove_http=True):
+	url = urlparse(url)
+	if Remove_http:
+		result = f"{url.netloc}"
+	else:
+		result = f"{url.netloc}://{url.netloc}"
+	return result
+
+def get_user_and_replace(url,user,x):
+	try:
+		if x in None:
+			return 0
+		if '[DOMAIN]' or '[UPPERUSER]' or '[DEFUSER]' in x:
+			if check_array(x):
+				new = []
+				for pwd in x:
+					new.append(pwd)
+			for px in new:
+				px.replace('[DOMAIN]',get_domain(url))
+				px.replace('[UPPERUSER]',user.upper())
+				px.replace('[DEFUSER]',user)
+			return px
+	except:
+		print(colored("seems we got error in function replace for wpbrute"))
