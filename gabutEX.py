8ba7801f56ddc06e9215a4054541dc1be4c56959
apaajalahx <52324987+apaajalahx@users.noreply.github.com>
2020-03-31 16:41:27 +0800

    Update gabutEX.py

--- gabutEX.py
+++ gabutEX.py
@@ -10,6 +10,11 @@ import argparse
 from concurrent.futures import ThreadPoolExecutor
 from __future__ import print_function
 from galer.module import check_cms
+from galer.wpbrute import go_brute
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'}
+month = time.strftime("%m")
+years = time.strftime("%Y")
 
 def banner():
 	print("""_  _ ____ _  _ ___ ____ ____ ____ _ ___ 
@@ -18,20 +23,54 @@ def banner():
                                         
 GabutEX v1""")
 
+def default_run(lis):
+	try:
+		with ThreadPoolExecutor(max_workers=10) as Executor:
+			with open(lis, 'r') as target:
+				for s in target:
+					url = s.rstrip()
+					Executor.submit(check_cms,url)
+	except IOError as e:
+		print(colored("Unknow error.",'red'))
+		sys.exit()
+
+def _brute(l,p):
+		try:
+			if 'default' in p:
+				go_brute(l,'disini/wordlist.txt')
+			else:
+				go_brute(l,p)
+		except:
+			print(colored("Seems we got error while try brute force"),'red')
+
 def main():
 	try:
-		parse = argparse.ArgumentParser(description='GabutEX V1')
+		parse = argparse.ArgumentParser(description='WordPress Killer V1')
 		parse.add_argument("--list", help="List website target", required=True)
+		parse.add_argument("--brute", help="WordPress Brute Force")
+		parse.add_argument("--password", help="Costum Password with specific directory")
+		parse.add_argument("--kill", help="Just scan all and try with lucky")
+		parse.add_argument("--url", help="single target")
 		peler = parse.parse_args()
 		try:
-			with ThreadPoolExecutor(max_workers=10) as Executor:
-				with open(peler.list, 'r') as target:
-					for s in target:
-						url = s.rstrip()
-						Executor.submit(check_cms,url)
+			if peler.list:
+				if peler.brute:
+					if peler.password:
+						_brute(peler.list,peler.password)
+					else:
+						_brute(peler.list,"default")
+				elif peler.kill:
+					default_run(peler.list)
+			elif peler.url:
+				if peler.brute:
+					if peler.password:
+						_brute(peler.list,peler.password)
+					else:
+						_brute(peler.list,"default")
+				elif peler.kill:
+					default_run(peler.list)				
 		except IOError as e:
-			print(colored("Unknow error.",'red'))
-			sys.exit()
+			print(colored("Error"+e),'red')
 	except KeyboardInterrupt as e:
 		print(colored("UNknow error.",'red'))
 		sys.exit()		
